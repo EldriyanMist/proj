99ac2529ab9a3daf1077c325f2e3639ce8a7e54a
EldriyanMist <kenzhe.damir17@gmail.com>
2023-05-22 17:36:22 +0600

    Create connect.go

---
+++ pkg/store/postgres/connect.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnectToPostgreSQL(host, port, user, password, dbname string) (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
