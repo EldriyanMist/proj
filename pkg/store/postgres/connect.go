948efcf2b677bebfbf1f63898e2a4037a78c1bb4
EldriyanMist <kenzhe.damir17@gmail.com>
2023-05-22 17:37:20 +0600

    Update connect.go

--- pkg/store/postgres/connect.go
+++ pkg/store/postgres/connect.go
@@ -1,27 +1,31 @@
-package main
+package postgres
 
 import (
 	"database/sql"
 	"fmt"
-	"log"
 
 	_ "github.com/lib/pq"
 )
 
-func ConnectToPostgreSQL(host, port, user, password, dbname string) (*sql.DB, error) {
+// ConnectToDB устанавливает соединение с базой данных PostgreSQL
+func ConnectToDB(host, port, user, password, dbname string) (*sql.DB, error) {
+	// Формируем строку подключения
 	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
+	// Подключаемся к базе данных
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
 	}
 
+	// Проверяем соединение
 	err = db.Ping()
 	if err != nil {
 		db.Close()
 		return nil, err
 	}
 
+	// Возвращаем работающее соединение к базе данных
 	return db, nil
 }
